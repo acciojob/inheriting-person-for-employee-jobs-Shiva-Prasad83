72c7781b7df677aac6ec613e86d4958e51353534
Shiva-Prasad83 <shivaprasad.ch83@gmail.com>
2025-10-15 22:53:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ class Employee extends Person{
 		this.jobTitle=jobTitle;
 	}
 	jobGreet(){
-		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`);
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 	}
 }
 const person=new Person("Alice",25);
